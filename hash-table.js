49986635c8b73f860b1b8f815c4f571f4b8a7e0b
mertkaandev <mertkaandev@outlook.com>
2022-05-30 12:01:36 +0300

    hash table

---
+++ hash-table.js
@@ -0,0 +1,11 @@
+// Hash Table Data Structure
+
+const students = {
+   1: {id: 1, name: "Mert"},
+   2: {id: 2, name: "Ceren"},
+   3: {id: 3, name: "Deniz"},
+   4: {id: 4, name: "Ali"},
+   5: {id: 5, name: "Ece"}
+};
+
+console.log(students[4]);
